a334fccffe38a2e21b31003b2a636aeac6546cb0
ZYC <834511975@qq.com>
2018-09-24 19:35:35 +0800

    修改了一小下下

--- idea_git/src/itheima/com/Test_git.java
+++ idea_git/src/itheima/com/Test_git.java
@@ -3,5 +3,11 @@ package itheima.com;
 public class Test_git {
     public static void main(String[] args) {
         System.out.println("idea 真特么好玩");
+        System.out.println("就是这么666666");
+        System.out.println("就是这么666666");
+        System.out.println("就是这么666666");
+        System.out.println("就是这么666666");
+        System.out.println("就是这么666666");
+        System.out.println("就是这么666666");
     }
 }
